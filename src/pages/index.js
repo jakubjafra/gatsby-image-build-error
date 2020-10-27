e19d5252848768a34dcc7f2a6a3faeb547c3800a
Jakub Jafra <jakub.jafra@gmail.com>
2020-10-28 00:08:25 +0100

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  return (
+    <div>
+      <h1>Hello gatsby-image</h1>
+      <Img fixed={data.file.childImageSharp.fixed} />
+    </div>
+  )
 }
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "test.jpg" }) {
+      childImageSharp {
+        fixed(width: 125, height: 125) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
